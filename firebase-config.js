e7d65e16bab3aebef9c38c11b9edeae571ed6111
abdullahiisah09060-cell <abdullahiisah09060@gmail.com>
2026-06-28 22:10:13 +0100

    Add Firebase configuration for the project

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDJlOQ4vgvuJPzE6ZG56aFxAtX0PSxvOtI",
+  authDomain: "apexvault-investment.firebaseapp.com",
+  projectId: "apexvault-investment",
+  storageBucket: "apexvault-investment.firebasestorage.app",
+  messagingSenderId: "884037084154",
+  appId: "1:884037084154:web:b9ca0de1293527d38afa43"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
